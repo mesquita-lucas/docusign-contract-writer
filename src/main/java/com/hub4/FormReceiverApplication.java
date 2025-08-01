7f5cc263c6cd6a7b59a2462f90748256b145f564
Lucas de Mesquita <lucas.barros18@hotmail.com>
2025-07-31 22:43:41 -0300

    Deleted old modules

--- src/main/java/com/hub4/FormReceiverApplication.java
+++ src/main/java/com/hub4/FormReceiverApplication.java
@@ -1,10 +1,7 @@
 package com.hub4;
 
-import com.hub4.pdf.ContractBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class FormReceiverApplication {
